c343c0bd5d1e039727b845c5a48c090493272847
ちあき <chiaki@chiakinoMacBook-Air.local>
2024-04-23 21:14:35 +0900

    route MVC追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Article;
 
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function showList() {
+        $model = new Article();
+        $articles = $model->getList();
+
+        return view('list', ['articles' => $articles]);
+    }
 }
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Article extends Model
 {
     use HasFactory;
+
+    public function getList() 
+    {
+        $articles = DB::table('articles')->get();
+        return $articles;
+    }
 }
